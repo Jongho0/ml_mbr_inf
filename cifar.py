f19704d987267555aecf19a7fdf6a52c727561f5
jongho <jongho@alumni.stanford.edu>
2019-03-26 00:05:20 +0900

    refactoring the code

--- cifar.py
+++ cifar.py
@@ -1,34 +1,49 @@
-import keras
+import argparse
+# import keras
 import numpy as np
-from sklearn.utils import resample
+from sklearn.utils import resample, shuffle
 
+# TODO corr between overfitting and attack precision
+# TODO corr between # of shadows and attack precision
 LEARNING_RATE = 0.001
-EPOCH = 100
-# EPOCH = 5
-DATA_SIZE = 5000
-TRAINING_SIZE = 5000
-TEST_SIZE = 1000
-NUM_TARGET = 1
-NUM_SHADOW = 100
-# NUM_SHADOW = 3
 IN = 1
 OUT = 0
-VERBOSE = 0
+VERBOSE = 1
 
-def sample_data(train_data,test_data,num_sets):
+def load_cifar10(num_class=10):
+    import keras
+    # load the pre-shuffled train and test data
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    # rescale [0,255] --> [0,1]
+    x_train = x_train.astype('float32')/255
+    x_test = x_test.astype('float32')/255
+    # one-hot encoding for the labels
+    class_size = len(np.unique(y_train))
+    y_train = keras.utils.to_categorical(y_train, class_size)
+    y_test = keras.utils.to_categorical(y_test, class_size)
+    # reduce the class size
+    y_train = y_train[:,:num_class]
+    y_test = y_test[:,:num_class]
+
+    shuffle(x_train, y_train, random_state=0)
+    shuffle(x_test, y_test, random_state=0)
+    return (x_train, y_train), (x_test, y_test)
+
+def sample_data(train_data, test_data, training_size, test_size, num_sets):
     (x_train, y_train), (x_test, y_test) = train_data, test_data
     new_x_train, new_y_train = [], []
     new_x_test, new_y_test = [], []
-    for i in range(num_sets):
-        x_temp, y_temp = resample(x_train, y_train, n_samples=TRAINING_SIZE, random_state=0)
+    for _ in range(num_sets):
+        x_temp, y_temp = resample(x_train, y_train, n_samples=training_size, random_state=0)
         new_x_train.append(x_temp)
         new_y_train.append(y_temp)
-        x_temp, y_temp = resample(x_test, y_test, n_samples=TEST_SIZE, random_state=0)
+        x_temp, y_temp = resample(x_test, y_test, n_samples=test_size, random_state=0)
         new_x_test.append(x_temp)
         new_y_test.append(y_temp)
     return (new_x_train, new_y_train), (new_x_test, new_y_test)
 
-def build_cnn_model():
+def build_cnn_model(num_class=10):
+    import keras
     from keras.models import Sequential
     from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
     # build the model
@@ -39,26 +54,44 @@ def build_cnn_model():
     model.add(MaxPooling2D(pool_size=2))
     model.add(Flatten())
     model.add(Dense(128, activation='tanh'))
-    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(64, activation='tanh'))
+    if num_class==1:
+        model.add(Dense(num_class, activation='sigmoid'))    
+    else:
+        model.add(Dense(num_class, activation='softmax')) 
     model.summary()
     return model
 
-def get_trained_keras_models(keras_model, train_data, test_data, num_models):
-    (x_train, y_train), (x_test, y_test) = train_data, test_data
+def get_keras_models(keras_model, num_class, num_models):
+    import keras
     models = []
     for i in range(num_models):
         models.append(keras.models.clone_model(keras_model))
-        models[i].compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-        models[i].fit(x_train[i], y_train[i], batch_size=32, epochs=EPOCH, verbose=VERBOSE, shuffle=True)
-        score = models[i].evaluate(x_test[i], y_test[i], verbose=VERBOSE)
-        print('\n', 'Model ', i, ' test accuracy:', score[1])
+        # rms = keras.optimizers.RMSprop(lr=LEARNING_RATE, decay=1e-7)
+        sgd = keras.optimizers.SGD(lr=LEARNING_RATE, decay=1e-7)
+        if num_class == 1 :
+            models[i].compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+        else:
+            models[i].compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     return models
+
+def train_keras_models(models, train_data, test_data, epochs):
+    (x_train, y_train), (x_test, y_test) = train_data, test_data
+    train_accs = []
+    test_accs = []
+    for i in range(len(models)):
+        history = models[i].fit(x_train[i], y_train[i], epochs=epochs, verbose=VERBOSE, shuffle=True, batch_size=32)
+        train_accs.append(history.history['acc'][-1])
+        score = models[i].evaluate(x_test[i], y_test[i], verbose=VERBOSE)
+        test_accs.append(score)
+        print('\n', 'Model ', i, ' test accuracy:', score[1])
+    return (train_accs, test_accs)
 
 def get_attack_dataset(models, train_data, test_data, num_models, data_size):
     # generate dataset for the attack model
     (x_train, y_train), (x_test, y_test) = train_data, test_data
-    num_classes = len(y_train[0][0])
-    x_data, y_data = [[] for i in range(num_classes)], [[] for i in range(num_classes)]
+    num_class = len(y_train[0][0])
+    x_data, y_data = [[] for _ in range(num_class)], [[] for _ in range(num_class)]
     for i in range(num_models):
         # IN data
         x_temp, y_temp = resample(x_train[i], y_train[i], n_samples=data_size, random_state=0)
@@ -74,55 +107,86 @@ def get_attack_dataset(models, train_data, test_data, num_models, data_size):
             y_data[y_idx].append(OUT)
     return x_data, y_data
 
-def get_trained_svm_models(train_data, test_data, num_models):
+def get_trained_svm_models(train_data, test_data):
     from sklearn import svm
     (x_train, y_train), (x_test, y_test) = train_data, test_data
+    num_models = len(y_train)
     models = []
+    score_sum = 0
     for i in range(num_models):
         print('Training svm model : ', i)
         models.append(svm.SVC(gamma='scale',kernel='linear',verbose=VERBOSE))
         models[i].fit(x_train[i], y_train[i])
         score = models[i].score(x_test[i],y_test[i])
+        score_sum = score_sum + score
         print('SVM model ', i, 'score : ',score)
+    print('Total attack score : ', score_sum/num_models)
     return models
 
-def main():
-    print('Hello World!')
-    # load the pre-shuffled train and test data
-    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-    # rescale [0,255] --> [0,1]
-    x_train = x_train.astype('float32')/255
-    x_test = x_test.astype('float32')/255
-    # one-hot encoding for the labels
-    num_classes = len(np.unique(y_train))
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
+def export_result():
+    # target model's training accuracy, test accuracy
+    # shadow models' training accuracy, test accuracy
+    # attack accuracy
+    pass
+
+def main(num_target=1, num_shadow=10, training_size=5000, test_size=1000, epochs=10, num_class=10): 
+    def split_pair(x_data, y_data, split_point):
+        assert len(x_data) == len(y_data)
+        assert len(x_data) > split_point
+        sp = split_point
+        return ((x_data[:sp],y_data[:sp]), (x_data[sp:],y_data[sp:]))
+
+    (x_train, y_train), (x_test, y_test) = load_cifar10(num_class)
 
     # split the data for each model
-    target_train = (x_train[:TRAINING_SIZE*NUM_TARGET],y_train[:TRAINING_SIZE*NUM_TARGET])
-    target_test = (x_test[:TEST_SIZE*NUM_TARGET],y_test[:TEST_SIZE*NUM_TARGET])
-    target_train_data, target_test_data = sample_data(target_train, target_test, NUM_TARGET)
+    split_point = training_size*num_target
+    train_data = split_pair(x_train, y_train, split_point)
+    split_point = test_size*num_target
+    test_data = split_pair(x_test, y_test, split_point)
 
-    shadow_train = (x_train[TRAINING_SIZE*NUM_TARGET:],y_train[TRAINING_SIZE*NUM_TARGET:])
-    shadow_test = (x_test[TEST_SIZE*NUM_TARGET:],y_test[TEST_SIZE*NUM_TARGET:])
-    shadow_train_data, shadow_test_data = sample_data(shadow_train, shadow_test, NUM_SHADOW)
+    target_train, target_test = sample_data(train_data[0], test_data[0], training_size, test_size, num_target)
+    shadow_train, shadow_test = sample_data(train_data[1], test_data[1], training_size, test_size, num_shadow)
 
-    cnn_model = build_cnn_model()
+    cnn_model = build_cnn_model(num_class)
     # compile the target model
-    target_models = get_trained_keras_models(cnn_model, target_train_data, target_test_data, NUM_TARGET)
+    target_models = get_keras_models(cnn_model, num_class, num_target)
+    train_keras_models(target_models, target_train, target_test, epochs)
     # compile the shadow models
-    shadow_models = get_trained_keras_models(cnn_model, shadow_train_data, shadow_test_data, NUM_SHADOW)
+    shadow_models = get_keras_models(cnn_model, num_class, num_shadow)
+    train_keras_models(shadow_models, shadow_train, shadow_test, epochs)
 
     # get train data for the attack model
-    attack_train = get_attack_dataset(shadow_models, shadow_train_data, shadow_test_data, NUM_SHADOW, TEST_SIZE)
+    attack_train = get_attack_dataset(shadow_models, shadow_train, shadow_test, num_shadow, test_size)
     # get test data for the attack model
-    attack_test = get_attack_dataset(target_models, target_train_data, target_test_data, NUM_TARGET, TEST_SIZE)
+    attack_test = get_attack_dataset(target_models, target_train, target_test, num_target, test_size)
 
     # training the attack model
-    attack_model = get_trained_svm_models(attack_train, attack_test, num_classes)
+    attack_model = get_trained_svm_models(attack_train, attack_test)
 
     # TODO generate the report
 
+def size_class_exp(num_shadow=10, epochs=10):
+    for training_size in [2500, 5000, 10000, 15000]:
+        for num_class in range(1,11):
+            main(1,num_shadow,training_size,1000,epochs,num_class)
+
+def shadow_num_exp(training_size=5000, test_size=1000, epochs=10, num_class=10):
+    for num_shadow in [1,10,50,100]:
+        main(1,num_shadow,training_size,test_size,epochs,num_class)
+
+def overfitting_exp(num_shadow=10,training_size=5000, test_size=1000, num_class=10):
+    for epochs in [10, 50, 100, 200, 500]:
+        main(1,num_shadow,training_size,test_size,epochs,num_class)
 
 if __name__== '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Member inference attack experiment for CIFAR10')
+    parser.add_argument('--num_target', type=int, default=1)
+    parser.add_argument('--num_shadow', type=int, default=10)
+    parser.add_argument('--training_size', type=int, default=5000)
+    parser.add_argument('--test_size', type=int, default=1000)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--num_class', type=int, default=10)
+    args = parser.parse_args()
+    main(args.num_target, args.num_shadow, args.training_size, args.test_size, args.epochs, args.num_class)
+
+    
